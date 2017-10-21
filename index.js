7d0443094a073c15da9920efe5a5a10a1a98eec5
Rahul <rahulprabhakar0596@gmail.com>
2017-10-21 14:23:18 +0530

    UI improvement

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Bounce{
         this.scoreboard();
         this.count = 0;
         this.score = 0;
-        this.time = 30;
+        this.time = 50;
         this.y = 0;
         this.x = 0;
         this.timer;
@@ -22,7 +22,7 @@ class Bounce{
         let scoreboard = document.createElement('h1');
         let timer = document.createElement('h2');
         timer.id = "timer";
-        timer.appendChild(document.createTextNode("00:30"));
+        timer.appendChild(document.createTextNode("Time Left: 00:50"));
         scoreboard.id = "score-board";
         let points = document.createTextNode("Score: 0");
         scoreboard.appendChild(points);
@@ -46,51 +46,53 @@ class Bounce{
    moveBall(){
       this.interval = setInterval(()=>{
         let ball = document.getElementById('ball');
-        let stickTop = document.getElementById('stick').getBoundingClientRect().top-100;
-        let stickLeft = document.getElementById('stick').getBoundingClientRect().left;
+        let stick = document.getElementById('stick');
+        let stickTop = stick.offsetTop-100;
+        let stickLeft = stick.offsetLeft;
         let move = 5;
         let ballTop = ball.offsetTop;
         let ballLeft = ball.offsetLeft;
           
-          if(ballTop >= window.innerHeight-100 && ballTop <=window.innerHeight) this.y=1;
+          if(ballTop >= window.innerHeight-80 && ballTop <=window.innerHeight) 
+              this.y=1; //up
           if(ballTop >= 0 && ballTop <=5) 
-              this.y=0;
-          if(ballLeft >= window.innerWidth-100 && ballLeft<= window.innerWidth)
-              this.x=1;
+              this.y=0; //down
+          if(ballLeft >= window.innerWidth-70 && ballLeft<= window.innerWidth)
+              this.x=1; //left
           if(ballLeft >= 0 && ballLeft <=5)
-              this.x=0;
-        
-          if(this.y ==1){
+              this.x=0; //right
+          
+          if(this.y ==0){ 
+            ball.style.top =  (ballTop+move) +"px";
+            if(this.x==0)  
+                ball.style.left = (ballLeft+move) + "px";
+            else if(this.x==1)
+                ball.style.left = (ballLeft-move) + "px";
+           }
+          else if(this.y ==1){
             ball.style.top =  (ballTop-move) +"px";
             if(this.x==0)  
                 ball.style.left = (ballLeft+move) + "px";
             else if(this.x==1)
                  ball.style.left = (ballLeft-move) + "px";
           }
-          else if(this.y ==0){
-            ball.style.top =  (ballTop+move) +"px";
-            if(this.x==0)  
-                ball.style.left = (ballLeft+move) + "px";
-            else if(this.x==1)
-                 ball.style.left = (ballLeft-move) + "px";
-           }
+          
           if( (stickTop >= ballTop-20 && stickTop<=ballTop) && (ballLeft>=stickLeft && ballLeft<=stickLeft+250)){
                 this.score+=5;
                 this.scoreUpdate(this.score);
             } 
-       },20);
+       },5);
     
    }
     scoreUpdate(score){
        let points = document.getElementById('score-board');
-        points.innerHTML = `Score: ${score}`;
-        
+        points.innerHTML = `Score: ${score}`; 
     }
     gameTimer(){
         let time = document.getElementById('timer');
         this.timer = setInterval(()=>{
              this.time--;
-            time.innerHTML = `00:${this.time}`;
+            time.innerHTML = `Time Left: 00:${this.time}`;
             if(this.time == 0){
                 clearInterval(this.timer);
                 clearInterval(this.interval);
